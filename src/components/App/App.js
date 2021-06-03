6a4ea33ed1ddbff2a76992f61e6188454baff2c7
Natalia <bvadim630@gmail.com>
2021-06-03 21:45:39 +0300

    Подключение стилей

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import InputItem from '../InputItem/InputItem';
 import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
+import './App.css';
 
   const todoItem = 'Написать новое приложение';
 
@@ -19,8 +20,8 @@ import Footer from '../Footer/Footer';
        }
   	];
 return(
-  	<div>
-  	<h1>Важные дела:</h1>
+  	<div className="wrap">
+  	<h1 className="wrap__title">Важные дела:</h1>
   	<InputItem />
   <ItemList items={items} />
   <Footer count={6} />
